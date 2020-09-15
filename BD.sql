82601003eeb3b53bcae9d2e04be0848390a93967
iagamaliel <caleb1693@hotmail.com>
2020-09-15 11:31:40 -0600

    Agregando script de BD

---
+++ BD.sql
@@ -0,0 +1,84 @@
+﻿USE [Codisa]
+GO
+/****** Object:  Table [dbo].[Area]    Script Date: 1/28/2019 2:19:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Area](
+	[IdArea] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](100) NOT NULL,
+	[Descripcion] [varchar](2000) NULL,
+ CONSTRAINT [PK_Area] PRIMARY KEY CLUSTERED 
+(
+	[IdArea] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Empleado]    Script Date: 1/28/2019 2:19:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Empleado](
+	[IdEmpleado] [int] IDENTITY(1,1) NOT NULL,
+	[NombreCompleto] [varchar](100) NOT NULL,
+	[Cedula] [varchar](50) NULL,
+	[Correo] [varchar](100) NULL,
+	[FechaNacimiento] [date] NULL,
+	[FechaIngreso] [date] NOT NULL,
+	[IdJefe] [int] NULL,
+	[IdArea] [int] NOT NULL,
+	[Foto] [varbinary](max) NULL,
+ CONSTRAINT [PK_Empleado] PRIMARY KEY CLUSTERED 
+(
+	[IdEmpleado] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Empleado_Habilidad]    Script Date: 1/28/2019 2:19:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Empleado_Habilidad](
+	[IdHabilidad] [int] IDENTITY(1,1) NOT NULL,
+	[IdEmpleado] [int] NOT NULL,
+	[NombreHabilidad] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_Empleado_Habilidad] PRIMARY KEY CLUSTERED 
+(
+	[IdHabilidad] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Area] FOREIGN KEY([IdArea])
+REFERENCES [dbo].[Area] ([IdArea])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Empleado] CHECK CONSTRAINT [FK_Empleado_Area]
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Empleado] FOREIGN KEY([IdJefe])
+REFERENCES [dbo].[Empleado] ([IdEmpleado])
+GO
+ALTER TABLE [dbo].[Empleado] CHECK CONSTRAINT [FK_Empleado_Empleado]
+GO
+ALTER TABLE [dbo].[Empleado_Habilidad]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Habilidad_Empleado] FOREIGN KEY([IdEmpleado])
+REFERENCES [dbo].[Empleado] ([IdEmpleado])
+GO
+ALTER TABLE [dbo].[Empleado_Habilidad] CHECK CONSTRAINT [FK_Empleado_Habilidad_Empleado]
+GO
